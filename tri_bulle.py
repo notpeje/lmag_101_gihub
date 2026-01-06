142f43e8a2eeec7d1d842ac93a2cc738a0628e82
notpeje <pierrejean.herrera@gmail.com>
2026-01-06 19:31:15 +0100

    Update tri_bulle.py

--- tri_bulle.py
+++ tri_bulle.py
@@ -4,7 +4,7 @@ def tri_bulle(liste):
     n = len(liste)
     for i in range(n):
         for j in range(0, n-i-1):
-            if liste[j] < liste[j+1]:
+            if liste[j] > liste[j+1]:
                 liste[j], liste[j+1] = liste[j+1], liste[j]
     return liste
 
